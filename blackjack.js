d2585a79584519b558e492a6392fc47553d5f864
Megha Sahgal <meghasahgal@Meghas-MacBook-Air.local>
2022-03-24 20:49:11 -0700

    modifications

--- blackjack.js
+++ blackjack.js
@@ -13,7 +13,7 @@ Cards are 2-11, with four 10s in each suit.
 
 Dealing needs to be wholly random, picking a random card from each suit and removing it from the object when drawn.
 
-Any number of players should be able to play, with a single dealer who draws last. 
+Any number of players should be able to play, with a single dealer who draws last.
 
 Multiple decks should be able to be added if so desired.
 
@@ -27,6 +27,7 @@ Add bidding????
 
 
 Side note, this would benefit from refactoring with classes... and testing to simplify this mess.*/
+let dealer = "Alex";
 let faceCards = {
     'J': 10,
     'Q': 10,
@@ -40,41 +41,51 @@ let deck = {
     Hearts: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
     Diamonds: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
     Clubs: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
-    
+
 };
 
 const numberOfPlayers = () => {
-    rl.question(`How many players are there?`, (answer) => {
-        if (!(isNaN(answer))){
-            console.log(`You responded ${answer} players. Is that correct?`);
-                rl.question(`Y/N`, (conf) => {
-                    if (conf.toLowerCase() === 'y' || conf.toLowerCase() === 'yes'){
-                        console.log(`Understood`);
-                        let playerCount = answer;
-                        playerRoster(playerCount);
-                    }
-                    else if (conf.toLowerCase() === 'n' ||  conf.toLowerCase() === 'no'){
-                        console.log(`Understood.`);
-                        numberOfPlayers();
-                    }
-                    else {
-                        console.log(`I'll take that as a 'no'.`);
-                        numberOfPlayers();
-                    }
-                });
-        }
-        else if (Number(answer) > 1){
+    rl.question(`How many players are there? `, firstQuestion)
+}
+// Refactoring the Above
+const firstQuestion = (answer) => {
+    if (!(isNaN(answer))){
+        console.log(`You responded ${answer} players. Is that correct?`);
+        rl.question(`Y/N `, response =>{
+                secondQuestion(response, answer)
+        })
+
+    }else if (Number(answer) > 1){
             console.log(`${answer} is too many players.`)
             numberOfPlayers();
 
-        }
-        else {
-            console.log(`I'm sorry, ${answer} isn't a number. Try again.`);
-            numberOfPlayers();
-        }
-    })
-};
+    }
+    else {
+        console.log(`I'm sorry, ${answer} isn't a number. Try again.`);
+        numberOfPlayers();
+    }
 
+}
+
+const secondQuestion = (conf, answer) =>{
+    console.log("conf:", conf)
+    if (conf.toLowerCase() === 'y' || conf.toLowerCase() === 'yes'){
+        console.log(`Understood `);
+        let playerCount = answer;
+        playerRoster(playerCount);
+    }
+    else if (conf.toLowerCase() === 'n' ||  conf.toLowerCase() === 'no'){
+        console.log(`Understood.`);
+        numberOfPlayers();
+    }
+    else {
+        console.log(`I'll take that as a 'no'.`);
+        numberOfPlayers();
+    }
+}
+
+
+numberOfPlayers()
 const handNumberizer = (hand) => {
     // this basically just lets us turn the original hand into numbers so it can be used for MATH.
     // there's probably a better way to do this but i suck at objects
@@ -128,7 +139,7 @@ const handSum = (hand) => {
 const cardDrawer = () => {
     let suitsArr = Object.keys(deck);
     let suit = suitsArr[Math.floor(Math.random() * suitsArr.length)]
-   
+
     if(deck[suit].length > 0){
         let deckSuit = deck[suit]
         let card = deckSuit[Math.floor(Math.random() * deckSuit.length)]
@@ -149,12 +160,12 @@ const dealACard = (hand) => {
     hand.push(cardArr[1]);
     deck[cardArr[0]] = deck[cardArr[0]].filter(element => !(element === cardArr[1]))
 };
-    
+
 const firstDeal = (roster) => {
     // calls numberOfDecks first.
     // anyway once the full deck is compiled...
     // Cards are dealt to each player in order of P1 -> PN -> D
-    // Each player gets two cards in that cycle. These are added to the roster object as an array of their numerical values. 
+    // Each player gets two cards in that cycle. These are added to the roster object as an array of their numerical values.
     // calls on cardDrawer each time it deals a card.
         for (let i = 0; i < 2; i++){
             for (let hand in roster){
@@ -163,21 +174,26 @@ const firstDeal = (roster) => {
         }
         // this... SHOULD cycle through it twice?
 };
-    
+
 let playerRoster = (playerCount, roster = {}) => {
     //so what we want to do is get each player's name, and shove each name into the object as its own key that we'll later use to store an array of cards as the values.
     //first things first, we need to iterate over the player count.
+    // Create a Recursive Function
     for (let i = 1; i <= playerCount; i++){
         rl.question(`Player ${i}, please enter your name.`, name => {
             roster[name] = []
-        });
+        }); // put this into a function and recursively call it
     };
     roster[dealer] = [];
-    console.log(roster);
+    //console.log("roster", roster);
     console.log(`Let's begin.`)
     firstDeal(roster);
+    //console.log("roster", roster)
 }
-    
+
+//playerRoster(3)
+
+
 //this should return an object containing blank arrays for each player hand, the keys being each player's name.
 
 // let numberOfDecks = (playerCount) => {
@@ -193,7 +209,7 @@ let playerRoster = (playerCount, roster = {}) => {
 
 let dealerTurn = (dealerHand) => {
 // ok
-    let dealerSum; 
+    let dealerSum;
     if (handSum(dealerHand) >= 17 && handSum(dealerHand) <= 21){
         dealerSum = handSum(dealerHand);
         console.log(`The Dealer stays.`);
@@ -214,7 +230,7 @@ let playerTurn = (playerHand) => {
     let playerSum;
     /* This will need functionality for multiple players, which means the roster object should also store a turn order to cycle through
     That is probably gonna be a pain in the ass.
-    
+
     Anyway, the base functionality of the player turn should be...
     idk basically the same as the dealer turn but with readline stuff
     */
@@ -232,9 +248,9 @@ let victory = () => {
     4. If a Player draws a 21 and the Dealer does not, the game ends immediately.*/
 };
 
-let testHand1 = ['A', 10, 2]
-let testHand2 = ['A', 'J']
-let testHand3 = ['J', 'Q', 'K']
-console.log(handSum(testHand1));
-console.log(handSum(testHand2));
-console.log(handSum(testHand3));
+// let testHand1 = ['A', 10, 2]
+// let testHand2 = ['A', 'J']
+// let testHand3 = ['J', 'Q', 'K']
+// console.log(handSum(testHand1));
+// console.log(handSum(testHand2));
+// console.log(handSum(testHand3));
